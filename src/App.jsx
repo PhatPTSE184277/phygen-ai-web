4f325bcdaf9525f34f55ea30512f76bfad2817ea
Truc Phuong <maitrucphuong0111@gmail.com>
2025-05-31 12:56:17 +0700

    home layout

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import HomePage from "./pages/home";
+
+function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <HomePage />,
+    },
+  ]);
+  return <RouterProvider router={router} />;
+}
+
+export default App;
---
+++ src/components/header/index.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./index.scss"; // Assuming you have a CSS file for styles
+
+function Header() {
+  return (
+    <header className="header">
+      <nav className="header__nav">
+        <h3>EXAMIFY</h3>
+        <div className="header__nav__items">
+          <ul>
+            <li className="nav__btn">
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/">About</Link>
+            </li>
+            <li>
+              <Link to="/">Blog</Link>
+            </li>
+            <li>
+              <Link to="/">Contact</Link>
+            </li>
+          </ul>
+          <div>
+            <button className="nav__button">Sign in</button>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;

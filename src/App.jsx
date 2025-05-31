341e0286c2c77a077911156c3225d86e6bee56e5
Truc Phuong <maitrucphuong0111@gmail.com>
2025-05-31 21:42:24 +0700

    SignIn Layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/home/home";
-
+import Login from "./pages/login/login";
+import Register from "./pages/register/register";
 
 function App() {
   const router = createBrowserRouter([
     {
       path: "/",
       element: <HomePage />,
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+    {
+      path: "/register",
+      element: <Register />,
     },
   ]);
   return <RouterProvider router={router} />;
---
+++ src/pages/register/register.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import LoginHeader from "../../components/login_header/login_header";
+
+function Register() {
+  return (
+    <div>
+      <LoginHeader></LoginHeader>
+    </div>
+  );
+}
+
+export default Register;

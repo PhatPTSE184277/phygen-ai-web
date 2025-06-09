53c0e6f5dc225257030ad6ee35d466e664afb8e0
Phạm Tấn Phát <tanphat120104@gmail.com>
2025-06-09 17:10:01 +0700

    fix antd overide

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,37 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+        assetFileNames: 'assets/[name].[hash].[ext]'
+      },
+    },
+    cssCodeSplit: false,
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: ``,
+        charset: false
+      }
+    },
+    devSourcemap: true,
+    postcss: {
+      plugins: []
+    }
+  },
+  resolve: {
+    alias: {
+      '@': '/src'
+    }
+  },
+  publicDir: 'public'
 })

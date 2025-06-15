39cebcfe7948ce944704aa7f817c9ee819f1e248
Truc Phuong <maitrucphuong0111@gmail.com>
2025-06-16 03:00:35 +0700

    generate/edit

---
+++ src/pages/user/generate-edit/index.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import Dashboard from "../../../components/dashboard";
+import { FormOutlined } from "@ant-design/icons";
+import "./index.scss";
+
+function GenerateEdit() {
+  const questions = Array.from({ length: 15 }, (_, i) => ({
+    id: i + 1,
+    text: `Question ${i + 1}: ngdaid aihiasd iadisa wueipaquei ijjasjdi`,
+    options: ["a. hiahas", "a. hiahas", "a. hiahas", "a. hiahas"],
+  }));
+  return (
+    <Dashboard>
+      <div className="exam-container">
+        <div className="exam-header">
+          <h2>My exam for the adsa ahdiad</h2>
+          <button className="finish-button">Finish</button>
+        </div>
+
+        <div className="exam-body">
+          {questions.map((q) => (
+            <div className="question-row" key={q.id}>
+              <p>{q.text}</p>
+              <p className="options">
+                {q.options.map((opt, i) => (
+                  <div className="option" key={i}>
+                    {opt}
+                  </div>
+                ))}
+
+                <div className="edit-icon">
+                  <FormOutlined />
+                </div>
+              </p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </Dashboard>
+  );
+}
+
+export default GenerateEdit;
